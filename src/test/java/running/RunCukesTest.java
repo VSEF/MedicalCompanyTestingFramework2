cc49464c59e59c27d074da67b594e1871716de8d
vladimir_grishau <sv.grishau@gmail.com>
2020-01-29 16:33:29 +0500

    tiny changes

--- src/test/java/running/RunCukesTest.java
+++ src/test/java/running/RunCukesTest.java
@@ -5,6 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "src/test/resources", glue = "src/test/java/homepage_steps/", plugin =  {"pretty","html:target/html", "json:cucumber.json"})
+@CucumberOptions(features= "src/test/fatures", plugin =  {"pretty","html:target/html", "json:cucumber.json"})
 public class RunCukesTest {
 }
+//glue = "src/test/java/homepage_steps/"
