634b8ea599f2955e67398f5341603b18c122eb41
vladimir_grishau <sv.grishau@gmail.com>
2020-01-29 17:20:24 +0500

    small restructuring

--- src/test/java/running/RunCukesTest.java
+++ src/test/java/running/RunCukesTest.java
@@ -5,7 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin =  {"pretty"})
+@CucumberOptions(features= "src/test/features",plugin =  {"pretty"})
 public class RunCukesTest {
 }
-//features= "src/test/features",
