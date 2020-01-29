5a40bf18aea4ef34b2fd4b2e337eac6ea5377446
vladimir_grishau <sv.grishau@gmail.com>
2020-01-29 12:20:32 +0500

    Project created

--- src/test/java/running/RunTests.java
+++ src/test/java/running/RunTests.java
@@ -1,8 +1,10 @@
 package running;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features= "features", glue = "steps", plugin =  {"pretty","html:target/html", "json:cucumber.json"})
 public class RunTests {
 }
